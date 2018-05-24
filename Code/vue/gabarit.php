eaab0bfa5778267b987ca6f7df02952bca473ea9
Catarina19 <Catarina.DE-JESUS@cpnv.ch>
2018-05-24 09:34:38 +0200

    CSS du site et journal de bord

--- Code/vue/gabarit.php
+++ Code/vue/gabarit.php
@@ -20,7 +20,7 @@
         </style>
     </head>
 
-    <body class="w3-content" style="max-width:1000px">
+    <body class="w3-content" style="max-width:100px">
     <!-- Sidebar/menu -->
     <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
         <div class="w3-container w3-display-container w3-padding-16">
@@ -64,19 +64,6 @@
 
             <?=$contenu ?>
 
-        </div>
-    </div>
-
-    <!-- Newsletter Modal -->
-    <div id="newsletter" class="w3-modal">
-        <div class="w3-modal-content w3-animate-zoom" style="padding:32px">
-            <div class="w3-container w3-white w3-center">
-                <i onclick="document.getElementById('newsletter').style.display='none'" class="fa fa-remove w3-right w3-button w3-transparent w3-xxlarge"></i>
-                <h2 class="w3-wide">NEWSLETTER</h2>
-                <p>Join our mailing list to receive updates on new arrivals and special offers.</p>
-                <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail"></p>
-                <button type="button" class="w3-button w3-padding-large w3-red w3-margin-bottom" onclick="document.getElementById('newsletter').style.display='none'">Subscribe</button>
-            </div>
         </div>
     </div>
 
